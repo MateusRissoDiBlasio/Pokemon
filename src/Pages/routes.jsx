68521459532fb0a8b3fee1ab711645c68d81b45c
unknown <mateusrisso@gmail.com>
2024-08-04 14:14:17 -0300

    commit

--- src/Pages/routes.jsx
+++ src/Pages/routes.jsx
@@ -3,7 +3,7 @@ import HomePage from "./home"
 import { CardDetailsPage } from "./cardDetailsPage"
 
 export const AppRoutes = () => (
-    <BrowserRouter  base href="./">
+    <BrowserRouter>
         <Routes>
             <Route exact path='/' element={<HomePage />} />
             <Route exact path='/details/:id' element={<CardDetailsPage />} />
