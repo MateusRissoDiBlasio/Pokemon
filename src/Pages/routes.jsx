f9a3074fedecd277e7c3108d12e88697968d6252
unknown <mateusrisso@yahoo.com.br>
2025-05-31 10:05:14 -0300

    evolution stages insertion, path correction.

--- src/Pages/routes.jsx
+++ src/Pages/routes.jsx
@@ -4,12 +4,23 @@ import { CardDetailsPage } from "./cardDetailsPage"
 
 export const AppRoutes = () => (
 
-    <BrowserRouter basename="/">
+
+// PARA ENVIAR AO GITHUB TEM Q SER DESSA MANEIRA 
+
+    <BrowserRouter basename="/Pokemon/">
         <Routes>
-            <Route exact path='/Pokemon/' element={<HomePage />} />
+            <Route exact path='/' element={<HomePage />} />
             <Route exact path='/details/:id' element={<CardDetailsPage />} />
         </Routes>
     </BrowserRouter>
 )
 
+// PARA REALIZAR ALTERAÇÕES LOCAIS 
 
+    // <BrowserRouter basename="/">
+    //     <Routes>
+    //         <Route exact path='/' element={<HomePage />} />
+    //         <Route exact path='/details/:id' element={<CardDetailsPage />} />
+    //     </Routes>
+    // </BrowserRouter>
+// )
