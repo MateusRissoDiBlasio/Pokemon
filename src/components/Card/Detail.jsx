7ae0afd75d273d480c6cd0234c31fa8233c8c618
unknown <mateusrisso@yahoo.com.br>
2025-06-28 23:04:28 -0300

    Swiper mobile size adjustment

--- src/components/Card/Detail.jsx
+++ src/components/Card/Detail.jsx
@@ -2809,7 +2809,6 @@ const EvolutionDiv = styled.div`
 
     @media screen and (min-width: 320px) and (max-width: 425px) and (max-height: 675px) {
       .swiper-wrapper {
-        background-color: aqua;
         margin-top: 22px;
       }
     }
