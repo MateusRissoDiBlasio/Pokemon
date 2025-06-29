0f49f27eb583313b6076284770dc64bd23e62e58
unknown <mateusrisso@yahoo.com.br>
2025-06-28 22:20:23 -0300

    Updates

--- assets/index-3VhvHrPR.js
+++ assets/index-3VhvHrPR.js
@@ -1676,7 +1676,7 @@ h4{
     @media screen and (min-width: 320px) and (max-width: 425px) and (max-height: 675px) {
       .swiper-wrapper {
         background-color: red;
-        margin-top: 0px;
+        margin-top: 30px;
       }
     }
 
